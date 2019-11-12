cd4a934a9b120def640b3e2d7d6cf9da97d8710c
nathanmailhiot <nathanjm12@outlook.com>
2019-11-12 12:17:25 -0500

    User can sign out

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,6 +42,11 @@ exports.signin = async (req, res) => {
   });
 };
 
+exports.signout = (req, res) => {
+  res.clearCookie("t");
+  return res.json({ message: "Signout Success!" });
+};
+
 exports.requireSignin = expressJwt({
   secret: process.env.JWT_SECRET,
   userProperty: "auth"
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
-// router.get("/signout", signout);
+router.get("/signout", signout);
 
 // any route containing :userId, our app will first execute userById()
 router.param("userId", userById);
