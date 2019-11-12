33a9d72142fda79a99862de06ba24607f866d6a3
nathanmailhiot <nathanjm12@outlook.com>
2019-11-12 12:00:54 -0500

    User signup

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-// const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
-// const expressValidator = require("express-validator");
-// const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -20,12 +20,16 @@ mongoose
 mongoose.connection.on("error", error => {
   console.log(`db connection error: ${error.message}`);
 });
+// Routes
+const authRoutes = require("./routes/authRoutes");
 
+// middleware
 app.use(morgan("dev"));
-// app.use(bodyParser.json());
-// app.use(cookieParser());
-// app.use(expressValidator());
-// app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+app.use("/", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello");
---
+++ controllers/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+require("dotenv").config();
+const expressJwt = require("express-jwt");
+const User = require("../models/user");
+
+exports.signup = async (req, res) => {
+  const userExists = await User.findOne({ email: req.body.email });
+  if (userExists)
+    return res.status(403).json({
+      error: "Email is taken!"
+    });
+  const user = await new User(req.body);
+  await user.save();
+  res.status(200).json({ message: "Signup success! Please login." });
+};
