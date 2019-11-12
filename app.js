c1f0f9241087b59214464cd69fe97b0f94f3661d
nathanmailhiot <nathanjm12@outlook.com>
2019-11-12 11:25:07 -0500

    Connected to mongoDB Atlas

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require("express");
-// const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+// const bodyParser = require("body-parser");
+// const cookieParser = require("cookie-parser");
+// const expressValidator = require("express-validator");
+// const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(console.log("DB connected"));
+
+mongoose.connection.on("error", error => {
+  console.log(`db connection error: ${error.message}`);
+});
+
 app.use(morgan("dev"));
+// app.use(bodyParser.json());
+// app.use(cookieParser());
+// app.use(expressValidator());
+// app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello");
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const uuidv1 = require("uuid/v1");
+const crypto = require("crypto");
+const { ObjectId } = mongoose.Schema;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  hashed_password: {
+    type: String,
+    required: true
+  },
+  salt: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
