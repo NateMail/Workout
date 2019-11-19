1936b2574ca9ff42cee68e069d85fe53c038063b
nathanmailhiot <nathanjm12@outlook.com>
2019-11-19 12:08:10 -0500

    Sign up

---
+++ client/src/components/auth/index.js
@@ -0,0 +1,14 @@
+export const signup = user => {
+  return fetch("http://localhost:8080/signup", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => console.log(error));
+};
