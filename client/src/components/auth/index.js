9bd7dc927421c7ba96019758a0c3677fbb7174a4
nathanmailhiot <nathanjm12@outlook.com>
2019-11-19 13:37:12 -0500

    Sign in

--- client/src/components/auth/index.js
+++ client/src/components/auth/index.js
@@ -12,3 +12,25 @@ export const signup = user => {
     })
     .catch(error => console.log(error));
 };
+
+export const signin = user => {
+  return fetch("http://localhost:8181/signin", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => console.log(error));
+};
+
+export const authenticate = (jwt, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(jwt));
+    next();
+  }
+};
