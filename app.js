d0344115ca4361f33b3f4e7c60fb02136c34df25
nathanmailhiot <nathanjm12@outlook.com>
2020-01-07 12:35:38 -0500

    Heroku

--- app.js
+++ app.js
@@ -43,6 +43,15 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 8181;
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
---
+++ config.js/production.js
@@ -0,0 +1,5 @@
+module.exports = {
+  redirectDomain: process.env.REDIRECT_DOMAIN,
+  mongoURI: process.env.MONGO_URI,
+  jwt: process.env.JWT_SECRET
+};
